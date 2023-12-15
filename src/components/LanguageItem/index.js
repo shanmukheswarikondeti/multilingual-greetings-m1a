c27c735195f970ffa6c93d77e5b690a6b4f801c0
shanmukheswarikondeti <eswarikondeti@gmail.com>
2023-12-15 15:09:53 +0000

    multilingual greetings

---
+++ src/components/LanguageItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const LanguageItem = props => {
+  const {isActive, onClickChangeLanguage, LanguageItemDetails} = props
+  const {buttonText, id} = LanguageItemDetails
+
+  const onClickButton = () => {
+    onClickChangeLanguage(id)
+  }
+  const activeTabBtnClassName = isActive ? 'highlighted' : 'not-highlighted'
+
+  return (
+    <li className="button-container">
+      <button
+        type="button"
+        className={`buttons ${activeTabBtnClassName}`}
+        onClick={onClickButton}
+      >
+        {buttonText}
+      </button>
+    </li>
+  )
+}
+export default LanguageItem
